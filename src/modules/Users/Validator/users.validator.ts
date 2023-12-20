8dbf5e44e581fb6c3e485a1537f993e34d9dbd5c
alireza yegane <alirezaporyegane@gmail.com>
2023-12-20 09:28:53 +0330

    feat: Add Validator Update Password And Add More Validation For Edit And Create

--- src/modules/Users/Validator/users.validator.ts
+++ src/modules/Users/Validator/users.validator.ts
@@ -1,5 +1,5 @@
 import joi from 'joi'
-import { CreateDtoIn } from '../Dto'
+import { CreateDtoIn, UpdateDtoIn } from '../Dto'
 
 export const userValidator = (data: Partial<CreateDtoIn>) => {
   const schema = joi.object<CreateDtoIn>({
@@ -9,14 +9,56 @@ export const userValidator = (data: Partial<CreateDtoIn>) => {
     email: joi.string().email().required(),
     phoneNumber: joi.string().required(),
     confirmedProfile: joi.boolean(),
-    emailConfirmed: joi.boolean(),
-    phoneNumberConfirmed: joi.boolean(),
+    confirmEmail: joi.boolean(),
+    confirmPhoneNumber: joi.boolean(),
+    role: joi.array().items(joi.string().required().valid('WRITER').valid('ADMIN').valid('USER')),
+    gender: joi.string().valid('MALE').valid('FEMALE').allow(null).allow(''),
     password: joi.string().required(),
     confirmPassword: joi.ref('password'),
     suspended: joi.boolean(),
     birthDate: joi.allow(null).allow(''),
+    legality: joi.number().integer().allow(null).allow(''),
     job: joi.allow(null).allow(''),
     nationalId: joi.allow(null).allow('')
+  })
+
+  return schema.validate(data)
+}
+
+export const userUpdateValidator = (data: Partial<UpdateDtoIn>) => {
+  const schema = joi.object<CreateDtoIn>({
+    userName: joi.string().required(),
+    firstName: joi.string().allow(null).allow(''),
+    lastName: joi.string().allow(null).allow(''),
+    email: joi.string().email().required(),
+    phoneNumber: joi.string().required(),
+    confirmedProfile: joi.boolean(),
+    confirmEmail: joi.boolean(),
+    confirmPhoneNumber: joi.boolean(),
+    role: joi.array().items(joi.string().required().valid('WRITER').valid('ADMIN').valid('USER')),
+    gender: joi.string().valid('MALE').valid('FEMALE').allow(null).allow(''),
+    suspended: joi.boolean(),
+    birthDate: joi.allow(null).allow(''),
+    legality: joi.number().integer().allow(null).allow(''),
+    job: joi.allow(null).allow(''),
+    nationalId: joi.allow(null).allow('')
+  })
+
+  return schema.validate(data)
+}
+
+export const userUpdatePasswordValidator = (
+  data: Partial<{
+    newPassword: string
+    confirmNewPassword: string
+  }>
+) => {
+  const schema = joi.object<{
+    newPassword: string
+    confirmNewPassword: string
+  }>({
+    newPassword: joi.string().required(),
+    confirmNewPassword: joi.ref('newPassword')
   })
 
   return schema.validate(data)
